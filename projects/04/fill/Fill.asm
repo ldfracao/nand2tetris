6bc657cdabf89e6484a01154af343304a37f44c3
ldfracao <ldfracao@protonmail.com>
2023-01-23 13:27:29 -0300

    finish project 4 with comments

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,92 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+
+(RESET)
+// the screen is 512 x 256, and each buffer has 16bits, so each column has 512/16 (32) sets of 16bits and 256 rows, so one can fit 32 X 256 = 8192 16bit sets into this buffer
+// counters for the "blackens" and "whitens" functions
+@8192
+D=A
+@count
+M=D
+@8192
+D=A
+@count2
+M=D
+
+// Declares screen_array variable which contains the address of the screen buffer
+@SCREEN
+D=A
+@screen_array
+M=D
+
+// offset variable used to sweep the entire screen buffer
+@i
+M=0
+@j
+M=0
+
+// loops until a key is pressed
+(INPUT)
+@KBD
+D=M
+@BLACKENS
+D;JGT
+@INPUT
+D;JEQ
+
+// when a key is pressed blackens the screen by writing -1 to the entire buffer, when no keys are pressed jumps to "whitens"
+(BLACKENS)
+@KBD
+D=M
+@WHITENS
+D;JEQ
+
+@screen_array
+D=M
+@i
+A=D+M
+M=-1
+
+@i // i++
+M=M+1
+
+@count //count--
+M=M-1
+D=M
+@RESET // jumps to the beginning when count reaches 0
+D;JEQ
+
+@BLACKENS
+D=M
+D;JGT
+
+// when no keys are pressed, keeps writing 0s to the screen buffer
+(WHITENS)
+@KBD
+D=M
+@BLACKENS
+D;JGT
+
+@screen_array
+D=M
+@j
+A=D+M
+M=0
+
+@j //j++
+M=M+1
+
+@count2 // jumps to the beginning when count2 reaches 0
+M=M-1
+D=M
+@RESET
+D;JEQ
+
+@WHITENS
+D=M
+D;JEQ
+
+(END)
+@END
+0;JMP
